1cd3d4b1ad186f71e9cdd1b95b014cff7e9ea835
Anton Shpakovych <shpakovych18@gmail.com>
2023-10-03 17:36:47 +0300

    modify admin panel for product

--- product/admin.py
+++ product/admin.py
@@ -3,5 +3,22 @@ from django.contrib import admin
 from product.models import Category, Product
 
 
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "category",
+        "is_offer_of_the_month",
+        "is_available",
+        "has_pickup_option",
+    )
+    list_filter = (
+        "category",
+        "is_offer_of_the_month",
+        "is_available",
+        "has_pickup_option",
+    )
+    search_fields = ("name", "description")
+
+
 admin.site.register(Category)
-admin.site.register(Product)
